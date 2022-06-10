0e8eb89a067a1e9abfd5637fdb0003fe38f4ee1a
Alonso Arriagada <45467026+Alobondo@users.noreply.github.com>
2022-06-10 17:17:56 -0400

    Add files via upload

---
+++ R/Q_sim.R
@@ -0,0 +1,9 @@
+#' Simulated daily flows
+#'
+#' A column containing values of simulated flow.
+#'
+#' @format A object with 13514 rows and 1 variable:
+#' \describe{
+#' \item{Q_sim}{simulations}
+#' }
+"Q_sim"
